624101acbd023ba59578e53155ca9bc00037a544
LoganSQL <loganchen@hotmail.com>
2019-06-26 15:56:36 -0400

    ACI guide

--- azure_container_instance.ps1
+++ azure_container_instance.ps1
@@ -52,12 +52,26 @@ az group delete --name myResourceGroup
 ################################################################################################
 # TASK TWO: Create Remote Container Registry
 #  => Push local image from local docker registry to remote container registry
+# Reference: http://techgenix.com/azure-container-registry/
 ################################################################################################
 az group create --name myResourceGroup --location eastus
 
+<#
+Azure Container Registry (ACR) is a private container registry that you can host your images in. 
+ACR works very nicely with ACI by allowing you to link the two together with minimal effort so you can host containers you built yourself.
+#>
+
 # create individual container registry
 az acr create --resource-group myResourceGroup --name logansqlcr --sku Basic --admin-enabled true
+<#
+This will create us a registry using the cheapest tier, Basic, but you can change the --sku to be Standard or Premium if you need. 
+The different sku’s mainly represent increased storage, with Premium also including geo replication. 
+We also set --admin-enabled true so that we can use a username/password to push to the registry, 
+alternatively, you can create a Service Principal and use that for authentication.
 
+From the JSON response, the most important piece of information is the loginServer, 
+we’ll need that when it comes to pushing images up to the registry.
+#>
 # show it: logansqlcr.azurecr.io
 az acr show --name logansqlcr --query loginServer --output table
 
@@ -86,6 +100,8 @@ logansqlcr.azurecr.io/node-alphine   v1                  49c4b7b2b458        21 
 az acr login --name logansqlcr
 
 # push the image from local docker registry to Azure Container Registry
+# By adding the repository prefix when we do a docker push 
+# Docker knows whether to push it to the public repository or to a 3rd party repository
 docker push logansqlcr.azurecr.io/node-alphine:v1
 <#
 PS C:\logan\test\nodeweb> docker push logansqlcr.azurecr.io/node-alphine:v1
@@ -118,14 +134,29 @@ Result
 v1
 #>
 
+# create an ACI that uses the registry.
+
 # show full name of container registry login server again
 # should be "logansqlcr.azurecr.io"
 az acr show --name logansqlcr --query loginServer
 
+<#
+ It’s time to create an ACI that uses the registry. 
+ Since we’ve enabled the admin account we need to get the password to login.
+
+ We’ve provided a --query "passwords[0].value" because there are two passwords 
+ and we only need one (there are two passwords so that there’s a backup should one be compromised and need resetting).
+#>
+az acr credential show --name logansqlcr --query "passwords[0].value"
+
+<#
+Ideally you’d want to assign this to a variable in your shell (bash/PowerShell/etc.) rather than writing it to stdout. 
+That’d avoid it ending up in the shells history and potentially being compromised.
+#>
 # deploy the container, like docker exec or docker start locally
-# --registry-username logansql@example.com --registry-password XXXXX
-# az container create --resource-group myResourceGroup --name node-alphine --image logansqlcr.azurecr.io/node-alphine:v1 --cpu 1 --memory 1 --registry-login-server logansqlcr.azurecr.io --dns-name-label logansqlnodeweb --ports 80
-az container create --resource-group myResourceGroup --name mycontainer --image mcr.microsoft.com/azuredocs/aci-helloworld --dns-name-label aci-logan --ports 80
+# 
+az container create --resource-group myResourceGroup --name node-alphine --image logansqlcr.azurecr.io/node-alphine:v1 --cpu 1 --memory 1 --registry-login-server logansqlcr.azurecr.io --registry-username logansqlcr --registry-password XXXXX --dns-name-label logansqlnodeweb --ports 80
+#az container create --resource-group myResourceGroup --name mycontainer --image mcr.microsoft.com/azuredocs/aci-helloworld --dns-name-label aci-logan --ports 80
 
 <#
 PS C:\logan\test\nodeweb> az container create --resource-group myResourceGroup --name mycontainer --image mcr.microsoft.com/azuredocs/aci-helloworld --dns-name-label aci-logan --ports 80
@@ -251,6 +282,27 @@ az container show --resource-group myResourceGroup --name mycontainer --query ip
 
 # View logs
 az container logs --resource-group myResourceGroup --name mycontainer
+
+<#
+Accessing Other Azure Services : building an application to run in ACI that needs to access another Azure Resource, maybe Azure SQL.
+ACI allows us to set environment variables. These work just as you’d expect coming from Docker and can be created as either normal environment variables 
+or secure environment variables. 
+The primary difference between the two is that a secure variable won’t appear in the ACI log or if you query the info of the container.
+
+create a SQL connection string for a web application, note that I’m not using a secure environment variable here:
+https://www.aaron-powell.com/posts/2019-03-20-intro-to-azure-container-instances/
+az container create --resource-group aci-from-scratch-03 --name aci-from-scratch-03 --image acifromscratch03.azurecr.io/aci-from-scratch-03:v1 --registry-login-server acifromscratch03.azurecr.io --registry-username acifromscratch03 --registry-password <password> --dns-name-label aci-from-scratch-03 --ports 80 --environment-variables 'SQLAZURECONNSTR_DefaultConnection'='Server=tcp:aci-from-scratch-03-sql.database.windows.net,1433;Database=aci-from-scratch;User ID=aci;Password=<sql password>;Encrypt=true;Connection Timeout=30;'
+#>
+$SQLCONNECTIONSTR='Server=tcp:aci-from-scratch-03-sql.database.windows.net,1433;Database=aci-from-scratch;User ID=aci;Password=<sql password>;Encrypt=true;Connection Timeout=30;'
+az container create --resource-group myResourceGroup --name node-alphine --image logansqlcr.azurecr.io/node-alphine:v1 --cpu 1 --memory 1 --registry-login-server logansqlcr.azurecr.io --registry-username logansqlcr --registry-password XXXXX --dns-name-label logansqlnodeweb --ports 80 --environment-variables 'SQLAZURECONNSTR_DefaultConnection'=$SQLCONNECTIONSTR
+
+<#
+The environment variables are --environment-variables and you can set multiple by using a space between them. 
+If you were to create a secret one then you’d use the --secrets option 
+(but be aware, they will appear in the CLI that executed the command, so you’re better using shell variables to insert them).
+
+If you’re planning to use secret variables you’re better off using a file deployment or Resource Manager template and inject the values into the file at runtime.
+#>
 
 # cleanup : save money
 az group delete --name myResourceGroup
